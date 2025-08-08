2fa7d57774c99143439b8479cb1801d2f0e5b821
Apialanag <a.fonse.p@gmail.com>
2025-08-08 18:39:58 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import BookingPage from './pages/BookingPage';
 import LoginPage from './pages/LoginPage';
 import AdminDashboardPage from './pages/AdminDashboardPage';
 import ProtectedRoute from './components/ProtectedRoute';
-import logo from './assets/logoapialan3.png';
+import logo from './assets/logoapialan4.png';
 
 function App() {
   const { authToken, logout } = useAuth();
